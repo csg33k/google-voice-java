8006be36ffdebebaa092e92ebd24d5bc3ca3b142
teisentraeger <teisentraeger@4b0ae8d0-c839-11de-8410-e5b1e99fc78e>
2010-03-14 13:04:00 +0000

    introduced the option to choose the different Tests
    git-svn-id: http://google-voice-java.googlecode.com/svn/trunk@35 4b0ae8d0-c839-11de-8410-e5b1e99fc78e

--- src/test/test.java
+++ src/test/test.java
@@ -9,11 +9,15 @@ import com.techventus.server.voice.Voice;
 import com.techventus.server.voice.datatypes.Phone;
 
 public class test {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static String userName = null;
+	static String pass = null;
+	
 	public static void main(String[] args){
 		System.out.println("Enter Your Google Voice Username, eg user@gmail.com:");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	      String userName = null;
+		
+    
 	      try {
 	         userName = br.readLine();
 	      } catch (IOException ioe) {
@@ -23,13 +27,16 @@ public class test {
 	      
 	      System.out.println("Enter Your Password:");
 	      
-	      String pass = null;
+	      
 	      try {
 	         pass = br.readLine();
 	      } catch (IOException ioe) {
 	         System.out.println("IO error trying to read your name!");
 	         System.exit(1);
 	      }
+	      
+	      listTests();
+	      
 	      /*
 	      System.out.println("Enter A \"Source\" for the Log:");
 	      String source = null;
@@ -50,56 +57,110 @@ public class test {
 	         System.exit(1);
 	      }
 	     */
-	      //test
-	      try {
+	      
+
+	}
+
+	/**
+	 * Lets the Tester choose the Test to run
+	 */
+	private static void listTests() {
+		System.out.println("Availible Tests:");
+		System.out.println("0: Exit");  
+		System.out.println("1: Multi phone enable / disable");
+		System.out.println("2: Inbox paging");
+
+		int testNr = 0;
+		try {
+			testNr = Integer.parseInt(br.readLine());
+		} catch (Exception e) {
+			System.out.println("Error trying to read the testNr!"+e.getMessage());
+			System.exit(1);
+		}
+		  
+		runTest(userName, pass, testNr);
+	}
+
+	/**
+	 * @param userName
+	 * @param pass
+	 * @param testNr
+	 */
+	private static void runTest(String userName, String pass, int testNr) {
+	    if(testNr==0) {// 0: Exit							
+			System.out.println("Exiting.");
+			System.exit(1);
+	    }
+		try {
 	    	  
 			Voice voice = new Voice(userName, pass);
 			//Voice voice = new Voice();
 			try {
 				System.out.println(voice.isLoggedIn());
 				//Thread.sleep(2000);
-				if(voice.phoneList!=null && voice.phoneList.size()>0) {
-					for(int i=0;i<voice.phoneList.size();i++){
-						System.out.println(voice.phoneList.get(i).toString());
+
+					switch (testNr) {
+						case 1: // 1: Multi phone enable / disable 
+							System.out.println("******** Starting Test "+testNr+" ********");
+							// create int Array from all phone ids
+							int[] phonesToChangeStatus = new int[voice.phoneList.size()];
+							int i=0;
+							
+							for (Iterator<Phone> iterator = voice.phoneList.iterator(); iterator.hasNext();) {
+								Phone type = (Phone) iterator.next();
+								phonesToChangeStatus[i] = type.id;
+								i++;
+							}
+							
+							System.out.println("Current phone status:");
+							if(voice.phoneList!=null && voice.phoneList.size()>0) {
+								for(int ii=0;ii<voice.phoneList.size();ii++){
+									System.out.println(voice.phoneList.get(ii).toString());
+								}
+							}
+							
+							//Disable all phones with one call
+							voice.phonesDisable(phonesToChangeStatus);
+							
+							// Output
+							System.out.println("After deactivate multi:");
+							voice = new Voice(userName, pass);
+							for(int ii=0;i<voice.phoneList.size();ii++){
+								System.out.println(voice.phoneList.get(ii).toString());
+							}
+							
+							//Enable all phones with one call
+							voice.phonesEnable(phonesToChangeStatus);
+							
+							// Output
+							System.out.println("After activate multi:");
+							voice = new Voice(userName, pass);
+							for(int ii=0;i<voice.phoneList.size();ii++){
+								System.out.println(voice.phoneList.get(ii).toString());
+							}
+							
+							System.out.println("******** Finished Test "+testNr+" ********");
+							break;
+							
+						case 2: // 2: Inbox paging
+							System.out.println("******** Starting Test "+testNr+" ********");
+							Thread.sleep(2000);
+							System.out.println(voice.getInboxPage(1000));
+							System.out.println("******** Finished Test "+testNr+" ********");
+							break;
+	
+						default: 						
+							System.out.println("Test "+testNr+" not found, exiting.");
+							System.exit(1);
+							break;
 					}
-					Thread.sleep(2000);
 					
-					// create int Array from all phone ids
-					int[] phonesToChangeStatus = new int[voice.phoneList.size()];
-					int i=0;
 					
-					for (Iterator<Phone> iterator = voice.phoneList.iterator(); iterator
-							.hasNext();) {
-						Phone type = (Phone) iterator.next();
-						phonesToChangeStatus[i] = type.id;
-						i++;
-					}
-					
-					//Disable all phones with one call
-					voice.phonesDisable(phonesToChangeStatus);
-					
-					// Output
-					System.out.println("After deactivate multi.");
-					voice = new Voice(userName, pass);
-					for(int ii=0;i<voice.phoneList.size();ii++){
-						System.out.println(voice.phoneList.get(ii).toString());
-					}
-					
-					//Enable all phones with one call
-					voice.phonesEnable(phonesToChangeStatus);
-					
-					// Output
-					System.out.println("After activate multi.");
-					voice = new Voice(userName, pass);
-					for(int ii=0;i<voice.phoneList.size();ii++){
-						System.out.println(voice.phoneList.get(ii).toString());
-					}
-				}
-				Thread.sleep(2000);
+//				Thread.sleep(2000);
 				
 				//System.out.println(voice.getSMS());
 				//System.out.println(voice.getInbox());
-				System.out.println(voice.getInboxPage(1000));
+//				System.out.println(voice.getInboxPage(1000));
 				//System.out.println(voice.getInboxPage(100));
 				/*
 				System.out.println(voice.getInbox());
@@ -135,18 +196,13 @@ public class test {
 				System.out.println("**********************************");
 				*/
 		
-			} catch (InterruptedException e) {
-				
+			} catch (InterruptedException e) {			
 				e.printStackTrace();
-			}
-			
-			
-		} catch (IOException e) {
-			
+			}		
+		} catch (IOException e) {	
 			e.printStackTrace();
 		}
-	      
-	      
-	      
+		listTests(); // List the Tests again
 	}
+	
 }
