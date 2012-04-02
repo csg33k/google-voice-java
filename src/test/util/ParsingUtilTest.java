50eed82a9536c9c578a14746f1be9f72482b31b0
biodroid1@gmail.com <biodroid1@gmail.com@4b0ae8d0-c839-11de-8410-e5b1e99fc78e>
2012-04-02 20:32:31 +0000

    simple unit tests for removeUniterestingParts
    git-svn-id: http://google-voice-java.googlecode.com/svn/trunk@181 4b0ae8d0-c839-11de-8410-e5b1e99fc78e

---
+++ src/test/util/ParsingUtilTest.java
@@ -0,0 +1,44 @@
+package test.util;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.techventus.server.voice.util.ParsingUtil;
+
+public class ParsingUtilTest {
+	
+	ParsingUtil parsingUtil;
+	
+	@SuppressWarnings("static-access")
+	@Test
+	public void testRemoveUninterestingPartsNullEndBorder() {
+		Assert.assertEquals(null, parsingUtil.removeUninterestingParts("Hello Toby, How are you today? Fine.", "How are", null, true));
+	}
+
+	@SuppressWarnings("static-access")
+	@Test
+	public void testRemoveUninterestingPartsNullStartBorder() {
+		Assert.assertEquals(null, parsingUtil.removeUninterestingParts("Hello Toby, How are you today? Fine.", null, "?", true));	
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test
+	public void testRemoveUninterestingPartsNullText() {
+		Assert.assertEquals(null, parsingUtil.removeUninterestingParts(null, "How are",  "?", true));
+	}
+		
+	@SuppressWarnings("static-access")
+	@Test
+	public void testRemoveUninterestingPartsIncludeBordersTrue() {
+		Assert.assertEquals("How are you today?", parsingUtil.removeUninterestingParts("Hello Toby, How are you today? Fine.", "How are", "?" , true));
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test
+	public void testRemoveUninterestingPartsIncludeBordersFalse() {
+		Assert.assertEquals(" you today", parsingUtil.removeUninterestingParts("Hello Toby, How are you today? Fine.", "How are", "?" , false));
+	}
+
+}
