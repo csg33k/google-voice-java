a41353774930a355d4c85da2289e26ba75d579d2
biodroid1@gmail.com <biodroid1@gmail.com@4b0ae8d0-c839-11de-8410-e5b1e99fc78e>
2012-04-02 17:53:07 +0000

    Fixed test to use reflection, reverted SMSParser.parsePhoneNumber to private. First time using reflection directly (not a mock) sorry if it's wordy.
    git-svn-id: http://google-voice-java.googlecode.com/svn/trunk@180 4b0ae8d0-c839-11de-8410-e5b1e99fc78e

--- src/test/util/SMSParserTest.java
+++ src/test/util/SMSParserTest.java
@@ -6,18 +6,31 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class SMSParserTest {
     private SMSParser testSMSParser;
+    private Method pPNM;
+    private static String TEST_PARSE_PHONE_NUMBER = "parsePhoneNumber";
+    @SuppressWarnings("rawtypes")
+	private Class[] parsePhoneNumberParameterTypes;
+    private Object[] parsePhoneNumberParameters;
 
     @Before
-    public void setup() {
-        testSMSParser = new SMSParser("test", "5030000000");
+    public void setup() throws Exception {
+        testSMSParser = new SMSParser("test", "5030000000"); 
+        parsePhoneNumberParameterTypes = new Class[1];
+        parsePhoneNumberParameterTypes[0] = java.lang.String.class;
+        pPNM = testSMSParser.getClass().getDeclaredMethod(TEST_PARSE_PHONE_NUMBER, parsePhoneNumberParameterTypes);
+        pPNM.setAccessible(true);
+        parsePhoneNumberParameters = new Object[1];
     }
 
     @Test
-    public void testParsePhoneNumber() {
-        //testSMSParser.parsePhoneNumber("5035551212");
-        Assert.assertEquals("+15035551212", testSMSParser.parsePhoneNumber("5035551212"));
+    public void testParsePhoneNumber() throws Exception {
+    	parsePhoneNumberParameters[0] = "5035551212";
+    	String test = (String) pPNM.invoke(testSMSParser, parsePhoneNumberParameters);
+        Assert.assertEquals("+15035551212", test);
     }
 
 
