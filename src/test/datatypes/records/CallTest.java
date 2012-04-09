8a5711a8f8600103228b5eccd7cd4b62d27e3a90
biodroid1@gmail.com <biodroid1@gmail.com@4b0ae8d0-c839-11de-8410-e5b1e99fc78e>
2012-04-09 17:16:16 +0000

    + Added unit tests for Call, Record, and ShortMessage
    git-svn-id: http://google-voice-java.googlecode.com/svn/trunk@188 4b0ae8d0-c839-11de-8410-e5b1e99fc78e

---
+++ src/test/datatypes/records/CallTest.java
@@ -0,0 +1,67 @@
+package test.datatypes.records;
+
+import java.util.Date;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.techventus.server.voice.datatypes.Contact;
+import com.techventus.server.voice.datatypes.records.Call;
+
+/**
+ * 
+ * @author bFutral
+ *
+ */
+public class CallTest {
+
+	//Constructors for 
+	final String ID = "testID";
+	final String TITLE = "testTitle";
+	final Date DATE = new Date(1321038671000l);
+	final Contact CONTACT = new Contact("testName", "testID", "testNumber", "testURL");
+	
+	Call testCall = new Call(ID, TITLE, DATE, CONTACT, true);
+	
+	@Test
+	public void testCallToStringOverrideFull() {
+		
+		Assert.assertEquals("id=testID;title=testTitle;date=Fri Nov 11 11:11:11 PST 2011;contact={id=testID;name=testName;number=testNumber;imageUrl=testURL;};read=true;", testCall.toString());	
+		
+	}
+	
+	@Test
+	public void testCallToStringOverrideNoID() {
+		
+		testCall.setId(null);
+		
+		Assert.assertEquals("title=testTitle;date=Fri Nov 11 11:11:11 PST 2011;contact={id=testID;name=testName;number=testNumber;imageUrl=testURL;};read=true;", testCall.toString());
+
+	}
+	
+	@Test
+	public void testCallToStringOverrideNoTitle() {
+		
+		testCall.setTitle(null);
+		
+		Assert.assertEquals("id=testID;date=Fri Nov 11 11:11:11 PST 2011;contact={id=testID;name=testName;number=testNumber;imageUrl=testURL;};read=true;", testCall.toString());
+	}
+	
+	@Test
+	public void testCallToStringOverrideNoDate() {
+		
+		testCall.setDate(null);
+		
+		Assert.assertEquals("id=testID;title=testTitle;contact={id=testID;name=testName;number=testNumber;imageUrl=testURL;};read=true;", testCall.toString());
+	}
+	
+	@Test
+	public void testCallToStringOverrideNoContact() {
+		
+		testCall.setContact(null);
+		
+		Assert.assertEquals("id=testID;title=testTitle;date=Fri Nov 11 11:11:11 PST 2011;read=true;", testCall.toString());
+	}
+
+}
---
+++ src/test/datatypes/records/RecordTest.java
@@ -0,0 +1,123 @@
+package test.datatypes.records;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.techventus.server.voice.datatypes.Contact;
+import com.techventus.server.voice.datatypes.records.Call;
+import com.techventus.server.voice.datatypes.records.Record;
+import com.techventus.server.voice.datatypes.records.ShortMessage;
+import com.techventus.server.voice.datatypes.records.Transcript;
+import com.techventus.server.voice.datatypes.records.TranscriptElement;
+import com.techventus.server.voice.datatypes.records.Voicemail;
+
+public class RecordTest {
+
+	Record testRecord;
+	Record testRecord1;
+	Record testRecord2;
+
+	final Contact CONTACT = new Contact("testName", "testID", "testNumber",
+			"testURL");
+
+	final Date DATE = new Date(1321038671000l);
+
+	final TranscriptElement.RecognitionLevel testLevel = TranscriptElement.RecognitionLevel.HIGH;
+
+	final TranscriptElement testElement = new TranscriptElement("testText",
+			"testID", testLevel);
+	
+	public List<TranscriptElement> testList = new ArrayList<TranscriptElement>();
+	
+	
+	@Before
+	public void setUp() {
+		
+		testList.add(testElement);
+		
+		final Transcript testTranscript = new Transcript(testList);
+		
+		testRecord = new Voicemail("ID1", "testTitle", DATE, CONTACT, testTranscript, true);
+		testRecord1 = new Call("ID1", "testTitle", DATE, CONTACT, true);
+		testRecord2 = new ShortMessage("ID1", "testTitle", DATE, CONTACT, true);
+	}
+
+	@Test
+	public void testIsVoicMailFalseCall() {
+
+		final boolean voicemailTestFalseCall = testRecord1.isVoicemail();
+
+		Assert.assertEquals(false, voicemailTestFalseCall);
+	}
+
+	@Test
+	public void testIsVoicMailFalseShortMessage() {
+
+		final boolean voicemailTestFalseShortMessage = testRecord2.isVoicemail();
+
+		Assert.assertEquals(false, voicemailTestFalseShortMessage);
+	}
+	
+	@Test
+	public void testIsVoicMailTrue() {
+
+		final boolean voicemailTesttrue = testRecord.isVoicemail();
+
+		Assert.assertEquals(true, voicemailTesttrue);
+	}
+
+	@Test
+	public void testIsCallFalseVoicemail() {
+
+		final boolean callTestFalseVoicemail = testRecord.isCall();
+
+		Assert.assertEquals(false, callTestFalseVoicemail);
+	}
+	
+	@Test
+	public void testIsCallFalseShortMessage() {
+
+		final boolean callTestFalseShortMessage = testRecord2.isCall();
+
+		Assert.assertEquals(false, callTestFalseShortMessage);
+	}
+
+	@Test
+	public void testIsCallTrue() {
+
+		final boolean callTestTrue = testRecord1.isCall();
+
+		Assert.assertEquals(true, callTestTrue);
+	}
+
+	@Test
+	public void testIsShortMessageFalseVoicemail() {
+
+		final boolean shortMessageTestFalseCall = testRecord.isShortMessage();
+
+		Assert.assertEquals(false, shortMessageTestFalseCall);
+	}
+	
+	@Test
+	public void testIsShortMessageFalseCall() {
+
+		final boolean shortMessageTestFalseCall = testRecord1.isShortMessage();
+
+		Assert.assertEquals(false, shortMessageTestFalseCall);
+	}
+
+	@Test
+	public void testIsShortMessageTrue() {
+
+		final boolean shortMessageTestTrue = testRecord2.isShortMessage();
+
+		Assert.assertEquals(true, shortMessageTestTrue);
+	}
+
+}
